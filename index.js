b01b5e6954b8ac4a301531ae46b3e63b73351d7a
daren farina <daren.farina@gmail.com>
2022-08-06 11:36:50 -0400

    commit tree d96e3ab0d671c0d2305e47894b8186e5dcd8a084

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(a) {
+    a = 2000
+    return a / 100
+    }
+
+const square = (paramater1) => paramater1 * paramater1
+
+const add = (paramater1, paramater2) => paramater1 + paramater2
